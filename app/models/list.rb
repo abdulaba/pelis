2e7452d14d51fd232fb47efc999af88648aec8c8
andres ibanez <andresibanezulloa@gmail.com>
2022-06-05 14:21:54 -0400

    add validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks , dependent: :destroy
   has_many :movies, through: :bookmarks
+
+  validates :name, presence: {message: "El nombre no puede estar vacío"}, uniqueness: true
 end
