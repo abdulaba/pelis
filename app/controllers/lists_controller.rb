c68288f099689bf2417ea71b4a023fd2fe55031e
andres ibanez <andresibanezulloa@gmail.com>
2022-06-05 18:07:32 -0400

    vistas controller list, partial

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,22 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render new
+    end
   end
 
   def edit
@@ -18,5 +26,10 @@ class ListsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
