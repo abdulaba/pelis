ebaf40c3e1cf9bf3a923a411bb465a620093c4e8
andres ibanez <andresibanezulloa@gmail.com>
2022-06-06 08:16:01 -0400

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_173733) do
+ActiveRecord::Schema.define(version: 2022_06_06_121523) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2022_06_05_173733) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_reviews_on_list_id"
+  end
+
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
+  add_foreign_key "reviews", "lists"
 end
